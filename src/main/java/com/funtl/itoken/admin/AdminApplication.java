398799c6563c6dffe8d341037619a366eed0c177
kyrie <1336861579@qq.com>
2019-08-11 20:48:45 +0800

    init commit

---
+++ src/main/java/com/funtl/itoken/admin/AdminApplication.java
@@ -0,0 +1,19 @@
+package com.funtl.itoken.admin;
+
+import de.codecentric.boot.admin.server.config.EnableAdminServer;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
+/**
+ * @author kyrie
+ * @date 2019/8/11 - 20:36
+ */
+@SpringBootApplication
+@EnableEurekaClient
+@EnableAdminServer
+public class AdminApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(AdminApplication.class, args);
+    }
+}
